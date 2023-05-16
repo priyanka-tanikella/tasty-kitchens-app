d58a4d9dbc2c83c9a8654106c09f0f266fbce00f
priyanka-tanikella <tanikellapriyanka123@gmail.com>
2023-05-16 08:21:49 +0000

    seventh commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -36,58 +36,53 @@ class Header extends Component {
       path === '/cart' ? 'cart-active-link-page' : 'cart-link-page'
     return (
       <>
-        <nav className="navbar-mobile-view">
-          <ul>
-            <li className="nav-item">
-              <Link to="/" className="link-item">
-                <div className="mobile-logo-and-title-container">
-                  <img
-                    src="https://res.cloudinary.com/denbtnhco/image/upload/v1681632796/Vectorlogo_prhtzg.png"
-                    alt="website logo"
-                    className="mobile-logo"
-                  />
-                  <h1 className="mobile-title">Tasty Kitchens</h1>
-                </div>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <div className="hamburger-menu-container">
+        <nav className="navbar-container">
+          <Link to="/" className="link-item">
+            <div className="mobile-logo-and-title-container">
+              <img
+                src="https://res.cloudinary.com/denbtnhco/image/upload/v1681632796/Vectorlogo_prhtzg.png"
+                alt="website logo"
+                className="mobile-logo"
+              />
+              <h1 className="mobile-title">Tasty Kitchens</h1>
+            </div>
+          </Link>
+
+          <div className="hamburger-menu-container">
+            <button
+              type="button"
+              className="hamburger-btn"
+              onClick={this.onClickHamburger}
+            >
+              <GiHamburgerMenu size="25" />
+            </button>
+          </div>
+
+          <div>
+            <ul className="home-cart-logout-container">
+              <li className="nav-item">
+                <Link to="/" className="link-item">
+                  <p className={homeClassName}> Home</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/cart" className="link-item">
+                  <p className={cartClassName}> Cart</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
                 <button
                   type="button"
-                  className="hamburger-btn"
-                  onClick={this.onClickHamburger}
+                  className="logout-button"
+                  onClick={this.onClickLogoutBtn}
                 >
-                  <GiHamburgerMenu size="25" />
+                  Logout
                 </button>
-              </div>
-            </li>
-
-            <li className="nav-item">
-              <div className="home-cart-logout-container">
-                <li className="nav-item">
-                  <Link to="/" className="link-item">
-                    <p className={homeClassName}> Home</p>
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link to="/cart" className="link-item">
-                    <p className={cartClassName}> Cart</p>
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <button
-                    type="button"
-                    className="logout-button"
-                    onClick={this.onClickLogoutBtn}
-                  >
-                    Logout
-                  </button>
-                </li>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </nav>
 
         {showMenu ? (
